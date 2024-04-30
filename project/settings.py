0d280123e25f08eb1b762f0fc8fd71d5742df0b1
Muhamed <muhamed.medhat11@gmail.com>
2024-04-30 15:09:46 +0300

    Add Blog, Accounts, About, Property and Settings Apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    ### my apps
+     
+    'property',
+    'blog',
+    'about',
+    'settings',
+    'accounts',
 ]
 
 MIDDLEWARE = [
